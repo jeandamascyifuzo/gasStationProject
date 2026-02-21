24f7828ba915ee70d1c09a3d75ef0c7972fe5642
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2026-02-21 11:57:51 +0200

    Add Escale.API, Escale.Web, and Escale.mobile projects

---
+++ Escale.mobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Escale.mobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Escale.mobile.Pages")]
